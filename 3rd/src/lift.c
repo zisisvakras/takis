9e20e1f272d97e75cc6c6d95890c93059776ebb6
Zisis Vakras <zisis.vakras@protonmail.com>
2023-01-13 00:14:07 +0200

    liftmem doc

--- 3rd/src/lift.c
+++ 3rd/src/lift.c
@@ -8,11 +8,15 @@ int main(void) {
     int* dests;
 
     /* Getting program input */
-    if(scanf("%d", &nrid) != 1) { /* Error case */
+    if(scanf("%d", &nrid) != 1) {
         fprintf(stderr, "Couldn't read input");
         return -1;
     }
-    if(scanf("%d", &nst) != 1) { /* Error case */
+    if(scanf("%d", &nst) != 1) {
+        fprintf(stderr, "Couldn't read input");
+        return -1;
+    }
+    if (nst < 0 || nst < 0) {
         fprintf(stderr, "Couldn't read input");
         return -1;
     }
@@ -20,13 +24,17 @@ int main(void) {
     /* Scanning for destinations */
     dests = malloc(nrid * sizeof(int));
 
-    if (dests == NULL) { /* Error case */
+    if (dests == NULL) {
         fprintf(stderr, "Couldn't allocate memory");
         return -1;
     }
 
     for (int i = 0 ; i < nrid ; i++) {
-        if(scanf("%d", dests + i) != 1) { /* Error case */
+        if(scanf("%d", dests + i) != 1) {
+            fprintf(stderr, "Couldn't read input");
+            return -1;
+        }
+        if (dests[i] < 0) {
             fprintf(stderr, "Couldn't read input");
             return -1;
         }
@@ -34,12 +42,13 @@ int main(void) {
 
     /* Solving the problem */
     int cost = solve(nrid, nst, dests);
-
-    if (cost == -1) { /* Error case */
+    
+    if (cost < 0) { /* Something happend */
         return -1;
     }
 
     /* Printing the cost */
+
     printf("Cost is: %d\n", cost);
 
     /* Free memory allocated for destinations */
--- 3rd/src/liftmem.c
+++ 3rd/src/liftmem.c
@@ -4,9 +4,10 @@
 
 /* Function described by the assignment */
 int fw(int start, int finish, int nrid, int* dests) {
-    int sum = 0; /* Initialize sum */
+    int sum = 0;
     if (finish == INFINITY) { 
-        /* If the right value is infinite we don't need to calculate 
+        /* 
+        *  If the second arg is infinity we don't need to calculate 
         *  infinity - dest because it will always be the larger one 
         */
         for (int i = 0 ; i < nrid ; i++) {
@@ -34,22 +35,22 @@ int fw(int start, int finish, int nrid, int* dests) {
 int M(int i, int j, int nrid, int* dests, int** mvalues) {
      /* In case i is 0 we need to execute the first formula described in the solution*/
     if (i == 0) {
-        if (mvalues[0][0] == INFINITY) {
+        if (mvalues[0][0] == INFINITY) { /* Catch the initial value */
             mvalues[0][0] = fw(0, INFINITY, nrid, dests);
         }
-        return mvalues[0][0];
+        return mvalues[0][0]; /* All M(0, j) is the same for all j */
     }
-    int min = INFINITY; /* Initializing the current min value */
+    int min = INFINITY;
     for (int k = 0 ; k <= j ; k++) { /* Using the sum described in solution */
         int sum = 0;
-        if (mvalues[i - 1][k] == INFINITY) {
+        if (mvalues[i - 1][k] == INFINITY) { /* Catch the initial value */
             mvalues[i - 1][k] = M(i - 1, k, nrid, dests, mvalues);
         }
         sum += mvalues[i - 1][k];
         sum -= fw(k, INFINITY, nrid, dests);
         sum += fw(k, j, nrid, dests);
         sum += fw(j, INFINITY, nrid, dests);
-        if (sum < min || min == INFINITY) { /* At the start we want to catch the infinite value */
+        if (sum < min || min == INFINITY) { /* At the start we want to catch the infinity value */
             min = sum;
         }
     }
@@ -58,9 +59,8 @@ int M(int i, int j, int nrid, int* dests, int** mvalues) {
 
 
 int solve(int nrid, int nst, int* dests) {
-    int nfl = 0; /* Declaring nfl variable*/
     int min = INFINITY; /* This holds the result of MinCost */
-    int floor = 0; /* This holds the last stop */
+    int nfl = 0, last_floor = 0;
 
     /* Looping through all destinations to find the highest floor */
     for (int i = 0 ; i < nrid ; i++) { 
@@ -74,8 +74,16 @@ int solve(int nrid, int nst, int* dests) {
     *  therefore we need an array with (nfl + 1)*(nst + 1) size like the following:
     */
     int** mvalues = malloc((nst + 1) * sizeof(int*));
+    if (mvalues == NULL) {
+        fprintf(stderr, "Couldn\'t allocate memory\n");
+        return -1;
+    }
     for (int h = 0 ; h <= nst ; h++) {
         mvalues[h] = malloc((nfl + 1) * sizeof(int));
+        if (mvalues[h] == NULL) {
+            fprintf(stderr, "Couldn\'t allocate memory\n");
+            return -1;
+        }
         for (int k = 0 ; k <= nfl ; k++) {
             mvalues[h][k] = INFINITY;
         }
@@ -84,18 +92,18 @@ int solve(int nrid, int nst, int* dests) {
     /* Executing the MinCost as described by the solution */
     for (int j = 0 ; j <= nfl ; j++) {
         int temp = M(nst, j, nrid, dests, mvalues);
-        if (temp < min || min == INFINITY) { /* At the start we want to catch the infinite value */
+        if (temp < min || min == INFINITY) { /* At the start we want to catch the infinity value */
             min = temp;
-            floor = j;
+            last_floor = j;
         }
     }
 
     /* Messages required by solution */
-    if (floor == 0) {
+    if (last_floor == 0) {
         printf("No lift stops\n");
     }
     else {
-        printf("Last stop at floor %d\n", floor);
+        printf("Last stop at floor %d\n", last_floor);
     }
 
     /* Freeing the 2d array */
